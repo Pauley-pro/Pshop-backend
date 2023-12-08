f48cbbc9abb25b7d11a1fd8eeef0dd0576005dbe
Paul Adelaja <pauladelaja54@gmail.com>
2023-12-08 13:58:15 +0100

    adjust app.js file

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const path = require("path");
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: "https://pshop-4i4f.onrender.com",
   credentials: true
 }));
 
